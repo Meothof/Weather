f117618eef75fe7f172e0478370779018cc27d9a
Théo Moffelein <tmoffelein@ludia.com>
2020-04-21 19:38:37 -0400

    Updated waether interface.

--- src/app/interfaces/weather.ts
+++ src/app/interfaces/weather.ts
@@ -9,7 +9,7 @@ export interface IWeatherDetails {
   dt: number;
   sunrise: number;
   sunset: number;
-  temp: number;
+  temp: any;
   feels_like: number;
   pressure: number;
   humidity: number;
@@ -21,11 +21,22 @@ export interface IWeatherDetails {
   weather: IWeatherOverview[];
 }
 
+export interface IWeatherForecast extends IWeatherDetails {
+  temp: {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn; number;
+  };
+}
+
 export interface IOneCallApiResponse {
   lat: number;
   lon: number;
   timezone: string;
   current: IWeatherDetails;
-  hourly: IWeatherDetails[];
-  daily: IWeatherDetails[];
+  hourly: IWeatherForecast[];
+  daily: IWeatherForecast[];
 }
