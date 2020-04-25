c9837ea3c145b17a6d4a3bb8d598a112a430903f
Theo Moffelein <theo.moffelein@gmail.com>
2020-04-25 18:23:18 -0400

    Updated PlaceRoutingModule.

--- src/app/place/place-routing.module.ts
+++ src/app/place/place-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlaceWeatherOverviewComponent } from './place-weather-overview/place-weather-overview.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'weather-overview',
     component: PlaceWeatherOverviewComponent
   }
 ];
