cf19a45e27c1e05e701642a2274ff0519846ef64
Theo Moffelein <theo.moffelein@gmail.com>
2020-04-26 13:29:16 -0400

    Created Degree to Cardinal pipe.

---
+++ src/app/pipes/degree-to-cardinal.pipe.ts
@@ -0,0 +1,46 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+enum Cardinals {
+  North = 'N',
+  NorthWest = 'NW',
+  West = 'W',
+  SouthWest = 'SW',
+  South = 'S',
+  SouthEast = 'SE',
+  East = 'E',
+  NorthEast = 'NE',
+}
+
+@Pipe({
+  name: 'degreeToCardinal'
+})
+export class DegreeToCardinalPipe implements PipeTransform {
+
+  transform(value: number): Cardinals {
+    if (value >= 337.5 && value < 360 || value >= 0 && value < 22.5) {
+      return Cardinals.North;
+    }
+    if (value >= 22.5 && value < 67.5) {
+      return Cardinals.NorthEast;
+    }
+    if (value >= 67.5 && value < 112.5) {
+      return Cardinals.East;
+    }
+    if (value >= 112.5 && value < 157.5) {
+      return Cardinals.SouthEast;
+    }
+    if (value >= 157.5 && value < 202.5) {
+      return Cardinals.South;
+    }
+    if (value >= 202.5 && value < 247.5) {
+      return Cardinals.SouthWest;
+    }
+    if (value >= 247.5 && value < 292.5) {
+      return Cardinals.West;
+    }
+    if (value >= 292.5 && value < 337.5) {
+      return Cardinals.NorthWest;
+    }
+  }
+
+}
--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecondsToMillisecondsPipe } from './seconds-to-milliseconds.pipe';
 import { TemperaturePipe } from './temperature.pipe';
+import { DegreeToCardinalPipe } from './degree-to-cardinal.pipe';
 
 @NgModule({
   declarations: [
     SecondsToMillisecondsPipe,
-    TemperaturePipe
+    TemperaturePipe,
+    DegreeToCardinalPipe,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     SecondsToMillisecondsPipe,
-    TemperaturePipe
+    TemperaturePipe,
+    DegreeToCardinalPipe,
   ]
 })
 export class PipesModule { }
